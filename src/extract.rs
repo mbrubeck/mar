8af7d8f7f6cd033fc12cbc44b1af712bca4aba52
Matt Brubeck <mbrubeck@limpet.net>
2017-10-07 14:51:06 -0700

    Add some comments

--- src/extract.rs
+++ src/extract.rs
@@ -2,12 +2,17 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+//! Extracting archives to the filesystem.
+
 use read::read_index;
 use std::io::{self, BufReader, ErrorKind, Read, Seek, SeekFrom};
 use std::fs::{File, OpenOptions};
 #[cfg(unix)] use std::os::unix::fs::OpenOptionsExt;
 use std::path::Path;
 
+/// Extract all the files from the specified archive to the current directory.
+///
+/// TODO: Accept an output path?
 pub fn extract<P: AsRef<Path>>(path: P) -> io::Result<()> {
     let mut archive = BufReader::new(File::open(path)?);
     let index = read_index(&mut archive)?;
@@ -16,6 +21,8 @@ pub fn extract<P: AsRef<Path>>(path: P) -> io::Result<()> {
         // Read data from the archive.
         archive.seek(SeekFrom::Start(item.offset as u64))?;
         let mut data = archive.by_ref().take(item.length as u64);
+
+        // TODO: Create parent directory if necessary.
 
         // Write data to a file.
         let mut options = OpenOptions::new();
